5e18cd2f2ae7fa1e4ab30ad3030fb15033c27b87
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-08-04 20:01:56 +0100

    remove signal listener

--- main.go
+++ main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"os"
-	"os/signal"
-	"syscall"
-
 	"github.com/bruno-anjos/scheduler/api"
 	utils "github.com/bruno-anjos/solution-utils"
 )
@@ -14,16 +10,5 @@ const (
 )
 
 func main() {
-	setupCloseHandler()
 	utils.StartServer(serviceName, api.DefaultHostPort, api.Port, api.PrefixPath, routes)
-}
-
-func setupCloseHandler() {
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-c
-		deleteAllInstances()
-		os.Exit(0)
-	}()
 }
